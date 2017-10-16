e314ab59bc728f7d78075f7f16bd1de26334967b
justincannizzaro <justincannizzaro777@gmail.com>
2017-10-15 20:02:08 -0400

    XML Layout Update 2 - Justin

--- app/src/main/java/franklin_practicum/f17gradebook/Authentication.java
+++ app/src/main/java/franklin_practicum/f17gradebook/Authentication.java
@@ -2,12 +2,69 @@ package franklin_practicum.f17gradebook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatButton;
+import android.support.v7.widget.AppCompatImageView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class Authentication extends AppCompatActivity {
+
+    private Button login, register;
+    private ImageView header;
+    private String screen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_authentication);
+
+        setMainScreen();
     }
+
+    public void setMainScreen()
+    {
+        setContentView(R.layout.activity_authentication);
+        login = (Button) findViewById(R.id.login);
+        register = (Button) findViewById(R.id.register);
+        header = (ImageView) findViewById(R.id.header);
+        screen = "main";
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.activity_authentication_login);
+                screen = "login";
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.activity_authentication_register);
+                screen = "register";
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(screen.equals("main"))
+        {
+            super.onBackPressed();
+        }
+        else
+        {
+            setMainScreen();
+        }
+    }
+    /*
+    //@Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.login:
+
+            break;
+        }
+    }
+    */
 }
--- app/src/main/java/franklin_practicum/f17gradebook/MainActivity.java
+++ app/src/main/java/franklin_practicum/f17gradebook/MainActivity.java
@@ -1,7 +1,9 @@
 package franklin_practicum.f17gradebook;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = new Intent(MainActivity.this, Authentication.class);
+        startActivity(intent);
     }
+
+    //@Override
+    //public void onClick(View v) {
+
+    //}
 }
