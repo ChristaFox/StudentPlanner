75c38360e047430f348c42e86bcd52c266d10200
fox65 <fox65@email.franklin.edu>
2017-10-17 15:05:32 -0700

    calendar

--- app/src/main/java/franklin_practicum/f17gradebook/Resources.java
+++ app/src/main/java/franklin_practicum/f17gradebook/Resources.java
@@ -1,13 +1,28 @@
 package franklin_practicum.f17gradebook;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Resources extends AppCompatActivity {
+    private Button linkToContacts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resources);
     }
+
+    contacts = (Button) findVeiwById(R.id.linkToContacts);
+
+        contacts.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            //if(validEmail && validPassword)
+            Intent intent = new Intent(Resources.this, Contacts.class);
+            startActivity(intent);
+        }
+    });
 }
