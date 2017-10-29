1ba97fc43c9921e350ee63c3c1ad43838e4d2ce3
fox65 <fox65@email.franklin.edu>
2017-10-29 10:24:42 -0700

    removed errors, so the gradle project syncs now. Mainly issues with layout parameters not available for the template, and had to remove constraint layout manually from the build manifest

--- app/src/main/java/franklin_practicum/f17gradebook/Contacts.java
+++ app/src/main/java/franklin_practicum/f17gradebook/Contacts.java
@@ -1,9 +1,37 @@
 package franklin_practicum.f17gradebook;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class Contacts extends AppCompatActivity {
+
+    private ArrayList<String> userName, email, phoneNum;
+
+    private void putArraysInIntent(Intent i){
+        i.putExtra("userName", userName);
+        i.putExtra("email", email);
+        i.putExtra("phoneNum", phoneNum);
+    }
+
+    public ArrayList<String> getContacts(){
+        return contacts;
+    }
+
+    public void setContacts(ArrayList<String> newList){
+        contacts = updatedContacts;
+    }
+
+    private void getArraysFromIntent(){
+        String[] UserNameArr = getIntent().getStringArrayExtra("userName");
+        String[] emailArr = getIntent().getStringArrayExtra("email");
+        String[] phoneNumArr = getIntent().getStringArrayExtra("phoneNum");
+        for(String s: UserNameArr){userName.add(s);}
+        for(String s: emailArr){email.add(s);}
+        for(String s: phoneNumArr){phoneNum.add(s);}
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
