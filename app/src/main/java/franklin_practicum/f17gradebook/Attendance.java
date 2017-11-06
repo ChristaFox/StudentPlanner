bb7e40219c79270f632ef590f2b14254f26ac8f5
fox65 <fox65@email.franklin.edu>
2017-11-05 19:50:43 -0800

    changes to attendence to have it work without the database

--- app/src/main/java/franklin_practicum/f17gradebook/Attendance.java
+++ app/src/main/java/franklin_practicum/f17gradebook/Attendance.java
@@ -3,11 +3,29 @@ package franklin_practicum.f17gradebook;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.PrintStream;
+import java.util.Scanner;
+
 public class Attendance extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_attendance);
+    }
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);  // Reading from System.in
+        PrintStream printf = System.out.printf("How many weeks is this course?");
+        int n = reader.nextInt(); // Scans the next token of the input as an int.
+        reader.close();
+        for (int i = 1; i < n; i++) {
+            PrintStream printf1 = System.out.printf("week %d", n);
+            //pull course number from database
+            //add course number to print out above
+            //for this feild the attends will be true/ boolean
+            // attendance = true;
+            System.out.println();
+        }
     }
 }
