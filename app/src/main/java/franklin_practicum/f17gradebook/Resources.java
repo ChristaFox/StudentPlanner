492743caff220ff635578a4fa8648dd4218fa29b
fox65 <fox65@email.franklin.edu>
2017-11-13 16:23:52 -0800

    resources

--- app/src/main/java/franklin_practicum/f17gradebook/Resources.java
+++ app/src/main/java/franklin_practicum/f17gradebook/Resources.java
@@ -51,7 +51,7 @@ public class Resources extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_resources);
+        setContentView(R.layout.activity_resources_list_item);
         getArraysFromIntent();
 
         resourcesListView = (ListView) findViewById(R.id.resourcesListView);
